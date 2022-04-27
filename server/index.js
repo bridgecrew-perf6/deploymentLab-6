5b3dae41fc6797e7883078ce72685a23a0baedfa
Skyler Trent <skyler.trent93@gmail.com>
2022-04-27 15:53:36 -0600

    testing

--- server/index.js
+++ server/index.js
@@ -3,12 +3,16 @@ const path = require('path')
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/html', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-app.get('/', (req, res) => {
+app.get('/css', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.css'))
+})
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.js'))
 })
 
 const port = process.env.PORT || 4005
