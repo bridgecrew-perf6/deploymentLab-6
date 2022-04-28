ff3a5e74ef15e9ac4819031cec71c708de6529ee
Skyler Trent <skyler.trent93@gmail.com>
2022-04-28 15:27:38 -0600

    function

--- server/index.js
+++ server/index.js
@@ -1,18 +1,24 @@
 const express = require('express')
 const path = require('path')
 
-const app = express()
 
 // include and initialize the rollbar library with your access token
-var Rollbar = require('rollbar')
-var rollbar = new Rollbar({
-  accessToken: '19b6ad7f2d784ba58232c5e9a6d4aacc',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
+const Rollbar = require('rollbar')
+let rollbar = new Rollbar({
+    accessToken: '19b6ad7f2d784ba58232c5e9a6d4aacc',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
 })
-
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
+
+const app = express()
+
+
+app.use(express.json())
+app.use('/style', express.static('../styles.css'))
+
+let bitcoins = []
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
@@ -26,7 +32,31 @@ app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.js'))
 })
 
+
+app.post('/api/bitcoin', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    const index = bitcoins.findIndex(bitcoinName=> bitcoinName === name)
+
+    if(index === -1 && name !== ''){
+        bitcoins.push(name)
+        rollbar.log('Reason added successfully', {author: 'Trent', type: 'manual entry'})
+        res.status(200).send(bitcoins)
+    } else if (name === ''){
+        rollbar.error('No reason given')
+        res.status(400).send('must provide a reason.')
+    } else {
+        rollbar.error('reason already exists')
+        res.status(400).send('that reason already exists')
+    }
+
+})
+
+
 const port = process.env.PORT || 4005
+
+app.use(rollbar.errorHandler())
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
